e9451761f2741060b8362134e1b27f5080ad9449
SeHIgh <sehi0119@naver.com>
2024-09-16 18:39:36 +0900

    gh-pages publish 관련하여 경로 오류로 인한 <BrowserRouter> 위치 변경 (Router.tsx -> App.tsx)

--- crypto-tracker/src/App.tsx
+++ crypto-tracker/src/App.tsx
@@ -1,18 +1,21 @@
 import Router from "./Router";
 import { Helmet } from "react-helmet-async";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyle from "./styles/GlobalStyle";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   // return <div><Router /></div>;
-  return <>
-    <Helmet>
-      <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" />
-    </Helmet>
-    <GlobalStyle />
-    <Router />
-    <ReactQueryDevtools />
-  </>
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" />
+      </Helmet>
+      <GlobalStyle />
+      <Router />
+      <ReactQueryDevtools />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- crypto-tracker/src/Router.tsx
+++ crypto-tracker/src/Router.tsx
@@ -7,15 +7,12 @@ import About from "./screens/About";
 
 function Router() {
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Routes>
-        {/* <Route path="/" element={<Home />} />
+    <Routes>
+      {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />}/> */}
-        <Route path="/:coinId/*" element={<Coin />} />
-        <Route path="/" element={<Coins />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/:coinId/*" element={<Coin />} />
+      <Route path="/" element={<Coins />} />
+    </Routes>
   );
 }
 
