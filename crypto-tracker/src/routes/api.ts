ebe48c4ce39124e7dd5769d74ead5a94bb076467
SeHIgh <sehi0119@naver.com>
2024-09-16 18:53:20 +0900

    gh-pages 의 Proxy-middleware 사용 불가 로 인하여 proxy 사용 없이 api 받아오기로 수정

--- crypto-tracker/src/routes/api.ts
+++ crypto-tracker/src/routes/api.ts
@@ -1,38 +1,38 @@
-// const BASE_URL = `https://api.coinpaprika.com/v1`;
+const BASE_URL = `https://api.coinpaprika.com/v1`;
 const NICO_URL = `https://ohlcv-api.nomadcoders.workers.dev`
 
-// export function fetchCoins() {
-//   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
-// }
-
-// export function fetchCoinInfo(coinId: string) {
-//   return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
-//     response.json()
-//   );
-// }
-
-// export function fetchCoinTickers(coinId: string) {
-//   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
-//     response.json()
-//   );
-// }
-
-// CORS Error Issue 로 인하여 proxy 이용
 export function fetchCoins() {
-  return fetch(`/api/coins`).then((response) => response.json());
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
 export function fetchCoinInfo(coinId?: string) {
-  return fetch(`/api/coins/${coinId}`).then((response) =>
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
     response.json()
   );
 }
 
 export function fetchCoinTickers(coinId?: string) {
-  return fetch(`/api/tickers/${coinId}`).then((response) =>
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
 }
+
+// CORS Error Issue 로 인하여 proxy 이용
+// export function fetchCoins() {
+//   return fetch(`/api/coins`).then((response) => response.json());
+// }
+
+// export function fetchCoinInfo(coinId?: string) {
+//   return fetch(`/api/coins/${coinId}`).then((response) =>
+//     response.json()
+//   );
+// }
+
+// export function fetchCoinTickers(coinId?: string) {
+//   return fetch(`/api/tickers/${coinId}`).then((response) =>
+//     response.json()
+//   );
+// }
 
 export function fetchCoinHistory(coinId?: string) {
   // 끝 날짜 : 현재 시간 -> 초 변환(JS 는 ms 로 반환) & 내림
